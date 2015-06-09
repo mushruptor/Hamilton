05a2d24c3bfe7131ccb4e2668e5073a481da660c
simeon <s8512927@mail.zih.tu-dresden.de>
2015-06-09 15:10:30 +0200

    fixed the output, the minisat solver works correctly with the generated output if you add the first line "p cnf 16 88" (16 variables, 88 conditions) refactored a bit...

--- hamilton.cc
+++ hamilton.cc
@@ -5,46 +5,67 @@
 using namespace std;
 
 
-int vertexnumber = 0;
+/*
+ * Internally I work with zero-based identifiers for nodes, positions and cnf variables.
+ * But since DIMACS works with one-based indentifiers, every input/output has to be decremented/incremented.
+ */
+
+
+struct edge {
+	int n1;
+	int n2;
+};
+
+
+bool equalEdge(edge e1, edge e2) {
+	return e1.n1 == e2.n1 && e1.n2 == e2.n2;
+}
+
+
+int nodenumber = 0;
 int cnfvarcount = 0;
 int cnfcount = 0;
-int currentedge = 1;
+edge currentedge;
 
 
 void writevariables1() {
-	cnfvarcount = vertexnumber * vertexnumber;
+	cnfvarcount = nodenumber * nodenumber;
 
-	for (int vertex = 0; vertex < vertexnumber; vertex++) {
-		// every vertex has to be in the path
-		for (int position = 0; position < vertexnumber; position++) {
-			int var = vertex * vertexnumber + position + 1;
+	// every node has to be in the path
+	for (int node = 0; node < nodenumber; node++) {
+		for (int position = 0; position < nodenumber; position++) {
+			int var = node * nodenumber + position + 1;
 			cout << var << " ";
 		}
 		cout << "0" << endl;
+	}
 
-		// no vertex can appear twice in the path
-		for (int position1 = 0; position1 < vertexnumber; position1++) {
-			for (int position2 = position1 + 1; position2 < vertexnumber; position2++) {
-				int var1 = vertex * vertexnumber + position1 + 1;
-				int var2 = vertex * vertexnumber + position2 + 1;
+	// no node can appear twice in the path
+	for (int node = 0; node < nodenumber; node++) {
+		for (int position1 = 0; position1 < nodenumber; position1++) {
+			for (int position2 = position1 + 1; position2 < nodenumber; position2++) {
+				int var1 = node * nodenumber + position1 + 1;
+				int var2 = node * nodenumber + position2 + 1;
 				cout << -var1 << " " << -var2 << " 0" << endl;
 			}
 		}
 	}
 
-	for (int position = 0; position < vertexnumber; position++) {
-		// every position has to be occupied by at least one vertex
-		for (int vertex = 0; vertex < vertexnumber; vertex++) {
-			int var = vertex * vertexnumber + position + 1;
+	// every position has to be occupied by at least one node
+	for (int position = 0; position < nodenumber; position++) {
+		for (int node = 0; node < nodenumber; node++) {
+			int var = node * nodenumber + position + 1;
 			cout << var << " ";
 		}
 		cout << "0" << endl;
+	}
 
-		// no position can be occupied by two vertices
-		for (int vertex1 = 0; vertex1 < vertexnumber; vertex1++) {
-			for (int vertex2 = vertex1 + 1; vertex2 < vertexnumber; vertex2++) {
-				int var1 = vertex1 * vertexnumber + position + 1;
-				int var2 = vertex2 * vertexnumber + position + 1;
+	// no position can be occupied by two nodes
+	for (int position = 0; position < nodenumber; position++) {
+		for (int node1 = 0; node1 < nodenumber; node1++) {
+			for (int node2 = node1 + 1; node2 < nodenumber; node2++) {
+				int var1 = node1 * nodenumber + position + 1;
+				int var2 = node2 * nodenumber + position + 1;
 				cout << -var1 << " " << -var2 << " 0" << endl;
 			}
 		}
@@ -54,51 +75,31 @@ void writevariables1() {
 }
 
 
-void writevariables2(int edge) {
-	edge = edge - 1;
-	int vertex1 = edge / vertexnumber;
-	int vertex2 = edge % vertexnumber;
-	if (vertex1 == vertex2) return;
-	// edge between vertex1 and vertex2 does not exist
-	for (int position = 0; position < vertexnumber; position++) {
-		int var1 = vertex1 * vertexnumber + position + 1;
-		int var2 = vertex2 * vertexnumber + ((position + 1) % vertexnumber) + 1;
+void writevariables2(edge e) {
+	int node1 = e.n1;
+	int node2 = e.n2;
+
+	for (int position = 0; position < nodenumber; position++) {
+		int var1 = node1 * nodenumber + position + 1;
+		int var2 = node2 * nodenumber + ((position + 1) % nodenumber) + 1;
 		cout << -var1 << " " << -var2 << " 0" << endl;
 	}
 }
 
 
-void handleline(string line) {
-	char c = line[0];
-	string word;
-	istringstream iss(line);
-	switch (c) {
-		case 'c':
-			break;
-		case 'p':
-			iss >> word; // should be "p"
-			iss >> word; // should be "edge"
-			iss >> word; // should be the number of vertices
-			vertexnumber = stoi(word);
-			writevariables1();
-			break;
-		case 'e':
-			iss >> word; // should be "e"
-			iss >> word; // should be the first vertex of the processed edge
-			int vertex1 = stoi(word) - 1;
-			iss >> word; // should be the second vertex of the processed edge
-			int vertex2 = stoi(word) - 1;
-
-			int edge = vertex1 * vertexnumber + vertex2 + 1;
-
-			while (currentedge < cnfvarcount && edge != currentedge) {
-				writevariables2(currentedge);
-				currentedge++;
-			}
-
-			break;
+bool incCurrentedge() {
+	currentedge.n2 = (currentedge.n2 + 1) % nodenumber;
+	if ( currentedge.n2 == 0 ) {
+		currentedge.n1 = (currentedge.n1 + 1) % nodenumber;
+		if ( currentedge.n1 == 0 ) {
+			return false;
+		}
 	}
-	return;
+
+	if ( currentedge.n1 == currentedge.n2 ) {
+		return incCurrentedge();
+	}
+	return true;
 }
 
 
@@ -108,20 +109,67 @@ main (int argc, char* argv[]) {
 		return 1;
 	}
 
+	// Start with the edge from node 0 to node 0. This will not be used as it is incremented before its first use.
+	currentedge.n1 = 0;
+	currentedge.n2 = 0;
+
 	string line; 
 	string filename = argv[1];
 	ifstream myfile (filename.c_str());
 	if (myfile.is_open()) {
-    	while ( getline (myfile,line) ) {
+    	
+    	bool processingEdges = false;
+    	while ( !processingEdges && getline (myfile,line) ) {
       		
-    		handleline(line);
+   			char c = line[0];
+
+    		switch (c) {
+    			case 'c': 
+    				continue;
+    			case 'p':
+    				string word;
+					istringstream iss(line);
+    				iss >> word; // should be "p"
+					iss >> word; // should be "edge"
+					iss >> word; // should be the number of nodes
+					nodenumber = stoi(word);
+					writevariables1();
+					processingEdges = true;
+					break;
+    		}
 
     	}
+
+    	while ( getline (myfile, line) ) {
+    		char c = line[0];
+
+    		if ( c == 'c' ) {
+    			continue;
+    		} else if ( c == 'e' ) {
+    			string word;
+				istringstream iss(line);
+    			iss >> word; // should be "e"
+				iss >> word; // should be the first node of the processed edge
+				int node1 = stoi(word) - 1;
+				iss >> word; // should be the second node of the processed edge
+				int node2 = stoi(word) - 1;
+
+				edge newedge;
+				newedge.n1 = node1;
+				newedge.n2 = node2;
+
+				while ( incCurrentedge() && !equalEdge(newedge, currentedge) ) {
+					writevariables2(currentedge);
+				}
+    		}
+
+    	}
+
+
     	myfile.close();
     	
-    	while (currentedge < cnfvarcount) {
+    	while (incCurrentedge()) {
 				writevariables2(currentedge);
-				currentedge++;
 		}
 
     } else cout << "Unable to open file" << endl; 
