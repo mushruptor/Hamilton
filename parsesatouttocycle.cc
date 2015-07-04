9b596b5dc7ae05f491c053a9585b74b8451ab32a
Simeon Kaul <simeon.kaul@mailbox.tu-dresden.de>
2015-07-04 15:37:32 +0200

    fixed bug in output parsing

--- parsesatouttocycle.cc
+++ parsesatouttocycle.cc
@@ -12,45 +12,50 @@ vector<int> variables;
 int nodenumber = 0;
 
 
-main (int argc, char* argv[]) {
+main (int argc, char* argv[]) 
+{
 	string line; 
 	string filename = argv[1];
 	ifstream myfile (filename.c_str());
 	
-	if (myfile.is_open()) {
-		
+	if (myfile.is_open()) 
+    {	
 		getline (myfile, line); // should be "SAT"
 		getline (myfile, line); // should be the solution of the SAT problem
 
 		string word;
 		istringstream iss(line);
     	iss >> word;
-    	int variable = stoi(word);
+    	long variable = stoi(word);
 
-    	while ( variable != 0 ) {
-    		variables.push_back(variable - 1);
+    	while ( variable != 0 ) 
+        {
+            if (variable > 0) 
+            {
+                variables.push_back(variable - 1);
+            }
+    		
     		iss >> word;
     		variable = stoi(word);
     	}
 
-    	nodenumber = round(sqrt(variables.size()));
-    	int * nodes;
-    	nodes = new int[nodenumber];
+    	nodenumber = variables.size();
+        long * nodes;
+    	nodes = new long[nodenumber];
 
-    	for (unsigned i = 0; i < variables.size(); i++) {
-    		if ( variables[i] > 0 ) {
-    			int position = variables[i] % nodenumber;
-    			int node = variables[i] / nodenumber;
-    			nodes[position] = node;
-    		}
+    	for (unsigned long i = 0; i < variables.size(); ++i) 
+        {
+            long position = variables[i] % nodenumber;
+            long node = variables[i] / nodenumber;
+  			nodes[position] = node;
     	}
 
         cout << "v ";
-    	for (unsigned j = 0; j < nodenumber - 1; j++) {
+    	for (unsigned long j = 0; j < nodenumber - 1; j++) 
+        {
     		cout << nodes[j] + 1 << " ";
     	}
         cout << nodes[nodenumber - 1] + 1 << endl;
-
 	}
 
 	return 0;
